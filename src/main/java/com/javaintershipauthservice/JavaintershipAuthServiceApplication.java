27072887b160b6ac2d314076b36429319e7424d1
maxonkizino <mkizino83@gmail.com>
2026-03-19 20:36:56 +0300

    feat: add auth service db schema feat: add model layer

--- src/main/java/com/javaintershipauthservice/JavaintershipAuthServiceApplication.java
+++ src/main/java/com/javaintershipauthservice/JavaintershipAuthServiceApplication.java
@@ -2,12 +2,15 @@ package com.javaintershipauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class JavaintershipAuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaintershipAuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaintershipAuthServiceApplication.class, args);
+    }
 
 }
