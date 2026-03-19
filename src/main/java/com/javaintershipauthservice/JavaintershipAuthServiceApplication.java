deff17fce53798f7aa0dad1cd205d224261ffd34
maxonkizino <mkizino83@gmail.com>
2026-03-19 15:50:30 +0300

    first commit

---
+++ src/main/java/com/javaintershipauthservice/JavaintershipAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javaintershipauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaintershipAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaintershipAuthServiceApplication.class, args);
+	}
+
+}
